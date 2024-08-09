865963b1b1a670b6ba745aada0cb8f85890743a4
Harold Manzano <hmanzano1012@gmail.com>
2024-08-09 17:32:02 -0600

    update dotfiles

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -95,6 +95,19 @@ local ui = {
 			indent = { char = "▏" },
 		},
 	},
+	{
+		"rachartier/tiny-devicons-auto-colors.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		event = "VeryLazy",
+		config = function()
+			local theme_colors = require("catppuccin.palettes").get_palette("macchiato")
+			require("tiny-devicons-auto-colors").setup({
+				colors = theme_colors,
+			})
+		end,
+	},
 }
 
 return ui
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -56,19 +56,6 @@ local utils = {
 		end,
 	},
 	{
-		"rachartier/tiny-devicons-auto-colors.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		event = "VeryLazy",
-		config = function()
-			local theme_colors = require("catppuccin.palettes").get_palette("macchiato")
-			require("tiny-devicons-auto-colors").setup({
-				colors = theme_colors,
-			})
-		end,
-	},
-	{
 		"kylechui/nvim-surround",
 		version = "*", -- Use for stability; omit to use `main` branch for the latest features
 		event = "VeryLazy",
